be0c55312442756c5a08ad018ad016b33569c40d
alexandre bouvard <alexandre.bouvard@outlook.com>
2019-04-06 23:52:20 -0400

    Page d'inscription

---
+++ Website/src/Controller/UsagerController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Controller;
+
+use App\Form\UsagerLoginType;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class UsagerController extends  AbstractController
+{
+    /**
+     * @Route( "/signin" , name="usager.login")
+     * @return Response
+     */
+    public function index(): Response {
+        $form= $this->createForm(UsagerLoginType::class);
+        return $this->render('pages/signin.html.twig',[
+            'form'=> $form->createView()
+        ]);
+    }
+
+}
