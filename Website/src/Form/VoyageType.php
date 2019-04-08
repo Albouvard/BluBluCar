b0f38d1a31473b7a8acbdc9071f48b02a5a5de85
Alexis <alexis.montorier@laposte.net>
2019-04-07 22:35:12 -0400

    amélioration vue formulaire création voyage

--- Website/src/Form/VoyageType.php
+++ Website/src/Form/VoyageType.php
@@ -4,6 +4,9 @@ namespace App\Form;
 
 use App\Entity\Voyage;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,10 +15,18 @@ class VoyageType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('horaire_at')
-            ->add('nbPlaces')
-            ->add('ville_depart')
-            ->add('ville_arrive')
+            ->add('horaire_at', DateTimeType::class,[
+                'label' => 'Horaire'
+            ])
+            ->add('nbPlaces', IntegerType::class, [
+                'label' => 'Nombre de places'
+            ])
+            ->add('ville_depart', TextType::class, [
+                'label' => 'Ville de départ'
+            ])
+            ->add('ville_arrive', TextType::class, [
+                'label' => 'Ville d\'arrivée'
+            ])
         ;
     }
 
